6974657c619998cd118546d501b8e0d106ed6513
vlu00 <vlu00@stuy.edu>
2018-12-04 20:11:17 -0500

    Removed driver from sorts

--- Sorts.java
+++ Sorts.java
@@ -12,11 +12,9 @@ public class Sorts{
   public static void selectionsort(int [] ary) {
     for (int i = 0; i < ary.length -1; i++) {
       int small = ary[i];
-      //int index = i;
       for (int n = i+1; n < ary.length; n++) {
         if (ary[n] < small) {
           small = ary[n];
-          //index++;
         }
       }
       int index = indexOf(small, ary);
@@ -25,12 +23,6 @@ public class Sorts{
       }
       ary[i] = small;
     }
-  }
-
-  public static void main (String [] args) {
-    int [] A = new int [] {5, 3, 7, 1, 6, 2, 4};
-    selectionsort(A);
-    System.out.println(Arrays.toString(A));
   }
 
 }
