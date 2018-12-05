dce7ad8457594324df01ffd8823e8df2d168b248
vlu00 <vlu00@stuy.edu>
2018-12-04 20:15:55 -0500

    Added comments for methods

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class Sorts{
+  //Find the index of the smallest value
   public static int indexOf(int x, int [] ary) {
     int index = 0;
     for (int i = 0; ary[i] != x; i++) {
@@ -12,16 +13,16 @@ public class Sorts{
   public static void selectionsort(int [] ary) {
     for (int i = 0; i < ary.length -1; i++) {
       int small = ary[i];
-      for (int n = i+1; n < ary.length; n++) {
+      for (int n = i+1; n < ary.length; n++) { //Ignoring already sorted values, find next smallest
         if (ary[n] < small) {
           small = ary[n];
         }
       }
       int index = indexOf(small, ary);
-      for (int k = index; k > i; k--) {
-        ary[k] = ary[k-1];
+      for (int k = index; k > i; k--) { //Moving from the index of the smallest value, move elements to the right until
+        ary[k] = ary[k-1];//you reach the already sorted elements
       }
-      ary[i] = small;
+      ary[i] = small; 
     }
   }
 
