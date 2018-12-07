bc86f91cab9dc381c64d8b5710928d6600ffd795
vlu00 <vlu00@stuy.edu>
2018-12-07 10:07:20 -0500

    bubbleSorts is written. Needs to be further tested and add a check for when length is 0.

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class Sorts{
+  //make sure you have a check for when ary length is zero
   public static void selectionSort(int [] ary) {
     for (int i = 0; i < ary.length-1; i++) {
       int old = ary[i];
@@ -17,5 +18,23 @@ public class Sorts{
       ary[oldindex] = old;
       ary[i] = small;
     }
+  }
+
+  public static void bubbleSort(int[] data) {
+    for (int i = 0; i < data.length-1; i++) {
+      for (int n = 0; n < data.length-i-1; n++) {
+        if (data[n] > data[n+1]) {
+          int x = data[n];
+          data[n] = data[n+1];
+          data[n+1] = x;
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int [] A = new int [] {5, 1, 12, -5, 16};
+    bubbleSort(A);
+    System.out.println(Arrays.toString(A));
   }
 }
