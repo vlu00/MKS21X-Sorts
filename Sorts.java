0b37908e14764730a0bced234a2a7eb7acbea374
vlu00 <vlu00@stuy.edu>
2018-12-07 21:00:10 -0500

    Tested and adjusted both selectionSort and bubbleSort. Added comments to bubbleSort

--- Sorts.java
+++ Sorts.java
@@ -1,41 +1,37 @@
 import java.util.Arrays;
 
 public class Sorts{
-  //make sure you have a check for when ary length is zero
   public static void selectionSort(int [] ary) {
-    for (int i = 0; i < ary.length-1; i++) {
-      int old = ary[i];
-      int small = ary[i];
-      int index = i;
-      int oldindex = i;
-      for (int n = i+1; n < ary.length; n++) { //Ignoring already sorted values, find next smallest
-        index++; //index of element checked increases
-        if (ary[n] < small) {
-          small = ary[n];
-          oldindex = index; //old index is the index of the smallest before it is move to the front
+    if (ary.length > 1) { //if length is 0 or 1, the ary remains the same
+      for (int i = 0; i < ary.length-1; i++) {
+        int old = ary[i];
+        int small = ary[i];
+        int index = i;
+        int oldindex = i;
+        for (int n = i+1; n < ary.length; n++) { //Ignoring already sorted values, find next smallest
+          index++; //index of element checked increases
+          if (ary[n] < small) {
+            small = ary[n];
+            oldindex = index; //old index is the index of the smallest before it is move to the front
+          }
         }
+        ary[oldindex] = old;
+        ary[i] = small;
       }
-      ary[oldindex] = old;
-      ary[i] = small;
     }
   }
 
   public static void bubbleSort(int[] data) {
-    for (int i = 0; i < data.length-1; i++) {
-      for (int n = 0; n < data.length-i-1; n++) {
-        if (data[n] > data[n+1]) {
-          int x = data[n];
-          data[n] = data[n+1];
-          data[n+1] = x;
-          System.out.println(Arrays.toString(data));
+    if (data.length > 1) {//if length is 0 or 1, data remains same
+      for (int i = 0; i < data.length-1; i++) { //for the length of data
+        for (int n = 0; n < data.length-i-1; n++) { //makes one pass through data checking elements next to one another
+          if (data[n] > data[n+1]) { //if first element is greater than second
+            int x = data[n];
+            data[n] = data[n+1]; //swap them
+            data[n+1] = x;
+          }
         }
       }
     }
-  }
-
-  public static void main(String[] args) {
-    int [] A = new int [] {5, 1, 12, -5, 16};
-    bubbleSort(A);
-    System.out.println(Arrays.toString(A));
   }
 }
