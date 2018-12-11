2450b05bc30b87628c8bbf742b80338413b6e33d
vlu00 <vlu00@stuy.edu>
2018-12-10 22:20:28 -0500

    Tested insertionSort. Fixed bubbleSort.

--- Sorts.java
+++ Sorts.java
@@ -20,52 +20,45 @@ public class Sorts{
       }
     }
   }
-//need to have a boolean that checks if any elements were swapped. If no elements were swapped then you can just end bubblesort.
+
   public static void bubbleSort(int[] data) {
     if (data.length > 1) {//if length is 0 or 1, data remains same
       for (int i = 0; i < data.length-1; i++) { //for the length of data
         for (int n = 0; n < data.length-i-1; n++) { //makes one pass through data checking elements next to one another
-          //if (swapped) {
-            //boolean swapped = false;
-            if (data[n] > data[n+1]) { //if first element is greater than second
-              int x = data[n];
-              data[n] = data[n+1]; //swap them
-              data[n+1] = x;
-              //swapped = true;
-            }
-          //}
+          boolean swapped = false;
+          if (data[n] > data[n+1]) { //if first element is greater than second
+            int x = data[n];
+            data[n] = data[n+1]; //swap them
+            data[n+1] = x;
+            swapped = true;
+          }
+          if (!swapped) {
+            i = data.length;
+          }
         }
       }
     }
   }
 
   public static void insertionSort(int [] data) {
-    if (data.length > 1) {
-      for (int i = 0; i < data.length-1; i++) {
-        int x = data[i+1];
+    if (data.length > 1) { //if length is 0 or 1, data remains the same
+      for (int i = 0; i < data.length-1; i++) { //for the length of data
+        int x = data[i+1]; //x is the number that needs to be sorted. Starts with one sorted already
         for (int n = i+1; n > 0; n--) {
-           if (n -1 == 0 && data[n-1] > x) {
-              data[n] = data[n-1];
-                 data[n-1] = x;
-              n=0;
+          if (n -1 == 0 && data[n-1] > x) { //if there is only one element left in "sorted" section and it is greater than x
+            data[n] = data[n-1]; //element in index 1 shifts to right
+            data[n-1] = x; //x becomes element at index 1
+            n=0; //exits loop
            }
-
-           else if (data[n-1] < x) {
-            data[n] = x;
-              n=0;
+          else if (data[n-1] < x) { //x is greater than next element in "sorted" side
+            data[n] = x; //place x in index after smaller number
+            n=0; //exits loop
           }
           else {
-            data[n] = data[n-1];
+            data[n] = data[n-1]; //element shifts to right
           }
         }
       }
     }
   }
-
-  public static void main(String[] args) {
-    int [] A = new int [] {5, 2, 3, 1, 0, 6};
-    insertionSort(A);
-    System.out.print(Arrays.toString(A));
-  }
-
 }
